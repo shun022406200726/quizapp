66edf3da4f82714876180c1bf1644876f9709d77
shun022406200726 <shun.085@gmail.com>
2024-08-06 15:31:24 +0900

    20240806_2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
 import './bootstrap';
+
+window.addEventListener('DOMContentLoaded',function(event){
+    let deleteButtons=document.querySelectorAll('.delete-quiz');
+
+    deleteButtons.forEach(deleteButton =>{
+        deleteButton.addEventListener('click',function(event){
+            let quizId=this.dataset.id;
+
+            if (!confirm('id:'+quizId+' のQuizを削除')) {
+                return;
+            }
+
+            axios.delete('/quizzes'+quizId).then(function(response){
+                let row=event.target.closest("tr");
+                row.parentNode.removeChild(row);
+
+                alert('削除')
+                console.log;
+            })
+
+            .catch(function (error) {
+                // リクエストで何らかのエラーが発生した場合
+                alert(error)
+                console.log(error);
+            });
+        });
+    });
+});
+
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/quizzes', function () {
+    return view('quizzes.index');
+});
+
+Route::get('/quizzes/show', function () {
+    return view('quizzes.show');
+});
+
+Route::get('/quizzes/create', function () {
+    return view('quizzes.create');
+});
