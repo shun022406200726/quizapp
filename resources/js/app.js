89591445893b641fa615dac01d6049c2d2b278b3
shun022406200726 <shun.085@gmail.com>
2024-08-08 08:42:12 +0900

    テンプレートの実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,30 +1,31 @@
 import './bootstrap';
 
-window.addEventListener('DOMContentLoaded',function(event){
-    let deleteButtons=document.querySelectorAll('.delete-quiz');
+window.addEventListener('DOMContentLoaded',function (event) {
+    let deleteButtons =document.querySelectorAll('.delete-quiz');
 
-    deleteButtons.forEach(deleteButton =>{
-        deleteButton.addEventListener('click',function(event){
-            let quizId=this.dataset.id;
+    deleteButtons.forEach(deleteButton=>{
+        deleteButton.addEventListener('click',function (event) {
+           let quizId=this.dataset.id;
+        });
 
-            if (!confirm('id:'+quizId+' のQuizを削除')) {
-                return;
-            }
+        if (!confirm('id:'+quizId+' のQuizを削除\nいい？')) {
+            return;
+        }
 
-            axios.delete('/quizzes'+quizId).then(function(response){
-                let row=event.target.closest("tr");
-                row.parentNode.removeChild(row);
+        axios.delete('/quizzes/'+quizId)
+            .then(function (response) {
+                let row =event.target.closest('tr');
+                row.parentNode.removeChild('row');
 
-                alert('削除')
-                console.log;
+                alert('削除');
+                console.log(response);
             })
-
-            .catch(function (error) {
-                // リクエストで何らかのエラーが発生した場合
+            .catch(function(error){
                 alert(error)
                 console.log(error);
-            });
-        });
+            })
+
+
     });
 });
 
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/quizzes/show', function () {
 Route::get('/quizzes/create', function () {
     return view('quizzes.create');
 });
+
+Route::delete('quizzes/{id}', function ($id){
+    return json_encode(['message'=>'ID:'.$id.'が削除されるIDです']);
+});
